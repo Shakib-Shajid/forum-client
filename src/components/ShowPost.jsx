5a60d994ee93977530c6ec05b42de2ee9fe4bfca
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-07-15 03:07:30 +0600

    update and delete button show who posts those

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Banner = ({ post }) => {
 
-    const { _id, name, position, desc, img } = post;
+    const { user } = useContext(AuthContext);
+
+    const { _id, name, position, desc, img, email } = post;
 
 
     const handleDelete = _id => {
@@ -17,6 +21,8 @@ const Banner = ({ post }) => {
                 }
             })
     }
+
+
     return (
         <div className="border-2 pl-2 md:pl-5 border-dashed relative border-gray-500 shadow-lg rounded-lg flex items-center  ">
             <img src={img} className="h-20 w-20 rounded-full border-2 border-blue-500 p-2 " alt="" />
@@ -24,10 +30,14 @@ const Banner = ({ post }) => {
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>{desc}</p>
-                    <div className="flex gap-5">
-                        <button onClick={() => handleDelete(_id)} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Delete Post</button>
-                        <Link to={`/updatePost/${_id}`} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Update Post</Link>
-                    </div>
+                    {user && 
+                        user.email === email ?
+                            <div className="flex gap-5">
+                                <button onClick={() => handleDelete(_id)} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Delete Post</button>
+                                <Link to={`/updatePost/${_id}`} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Update Post</Link>
+                            </div>
+                            : ""
+                    }
                 </div>
             </div>
             <Link to={`/peopleprofile/${_id}`} className="absolute top-0 right-0 btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Go Profile</Link>
