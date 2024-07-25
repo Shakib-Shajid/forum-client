493b66be3b6cc17b589d376121e047ee915f279e
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-07-26 00:23:54 +0600

    3 dot added in posts

--- src/components/ShowPost.jsx
+++ src/components/ShowPost.jsx
@@ -24,23 +24,44 @@ const Banner = ({ post }) => {
 
 
     return (
-        <div className="border-2 pl-2 md:pl-5 border-dashed relative border-gray-500 shadow-lg rounded-lg flex items-center  ">
-            <img src={img} className="h-20 w-20 rounded-full border-2 border-blue-500 p-2 " alt="" />
-            <div>
+        <div className="border-2 pl-2 md:pl-5 border-dashed relative border-gray-500 shadow-lg rounded-lg flex items-center ">
+                <img src={img} className="h-20 w-20 rounded-full border-2 border-blue-500 p-2" alt="" />
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
+                    <h2 className="card-title"><Link to={`/peopleprofile/${_id}`}>{name}</Link></h2>
                     <p>{desc}</p>
-                    {user && 
+                    {user &&
                         user.email === email ?
-                            <div className="flex gap-5">
-                                <button onClick={() => handleDelete(_id)} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Delete Post</button>
-                                <Link to={`/updatePost/${_id}`} className=" btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Update Post</Link>
+                        <div className="dropdown dropdown-bottom dropdown-end absolute top-0 right-0 mx-2">
+                            <div tabIndex={0} role="button" className="">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-8 h-8">
+                                    <path d="M2.25 12C2.25 10.4812 3.48122 9.25 5 9.25C6.51878 9.25 7.75 10.4812 7.75 12C7.75 13.5188 6.51878 14.75 5 14.75C3.48122 14.75 2.25 13.5188 2.25 12ZM5 10.75C4.30964 10.75 3.75 11.3096 3.75 12C3.75 12.6904 4.30964 13.25 5 13.25C5.69036 13.25 6.25 12.6904 6.25 12C6.25 11.3096 5.69036 10.75 5 10.75Z" fill="#1C274C" />
+                                    <path d="M9.25 12C9.25 10.4812 10.4812 9.25 12 9.25C13.5188 9.25 14.75 10.4812 14.75 12C14.75 13.5188 13.5188 14.75 12 14.75C10.4812 14.75 9.25 13.5188 9.25 12ZM12 10.75C11.3096 10.75 10.75 11.3096 10.75 12C10.75 12.6904 11.3096 13.25 12 13.25C12.6904 13.25 13.25 12.6904 13.25 12C13.25 11.3096 12.6904 10.75 12 10.75Z" fill="#1C274C" />
+                                    <path d="M19 9.25C17.4812 9.25 16.25 10.4812 16.25 12C16.25 13.5188 17.4812 14.75 19 14.75C20.5188 14.75 21.75 13.5188 21.75 12C21.75 10.4812 20.5188 9.25 19 9.25ZM17.75 12C17.75 11.3096 18.3096 10.75 19 10.75C19.6904 10.75 20.25 11.3096 20.25 12C20.25 12.6904 19.6904 13.25 19 13.25C18.3096 13.25 17.75 12.6904 17.75 12Z" fill="#1C274C" />
+                                </svg>
                             </div>
-                            : ""
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+
+                                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                    <li>
+                                        <Link to={`/updatePost/${_id}`}>Edit</Link>
+                                    </li>
+                                    <li>
+                                        <button onClick={() => handleDelete(_id)}>Delete</button>
+                                    </li>
+                                </ul>
+                            </ul>
+                        </div>
+                        : ""
                     }
+
+                    <div>
+
+                    </div>
+
+
+
                 </div>
-            </div>
-            <Link to={`/peopleprofile/${_id}`} className="absolute top-0 right-0 btn-xs lg:btn lg:btn-neutral text-black lg:text-white">Go Profile</Link>
+
         </div>
     );
 };
